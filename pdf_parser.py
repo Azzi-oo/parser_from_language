82a4307dba4c028a0b5bc602668e7680798753ff
Azzi-oo <yanberdin.az@yandex.ru>
2024-02-12 20:35:58 +0500

    add new changes

--- pdf_parser.py
+++
@@ -1,9 +0,0 @@
-import fitz  # PyMuPDF
-
-def extract_text_from_pdf(pdf_path):
-    text = ''
-    with fitz.open(pdf_path) as pdf_document:
-        for page_num in range(pdf_document.page_count):
-            page = pdf_document[page_num]
-            text += page.get_text()
-    return text
